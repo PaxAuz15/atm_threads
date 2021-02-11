36acecf8296c1aad8c09ae65acdd2ae7c6f25ee3
Luis Auz <luisauz.geek@gmail.com>
2021-02-10 21:15:23 -0500

    feat: simulacionRetiroClientes class's logic

--- src/client/simulacionRetiroClientes.java
+++ src/client/simulacionRetiroClientes.java
@@ -1,5 +1,22 @@
 package src.client;
 
+import src.model.Cuenta;
+import src.app.TitularDeLaCuenta;
+
 public class simulacionRetiroClientes{
-    
+    public static void main(String[] args) {
+        Cuenta cuenta = new Cuenta();
+        TitularDeLaCuenta titularDeLaCuenta = new TitularDeLaCuenta(cuenta);
+        Thread hilo1 = new Thread(titularDeLaCuenta);
+        Thread hilo2 = new Thread(titularDeLaCuenta);
+        Thread hilo3 = new Thread(titularDeLaCuenta);
+
+        hilo1.setName("Rosado");
+        hilo2.setName("Dannes");
+        hilo3.setName("Alberto");
+
+        hilo1.start();
+        hilo2.start();
+        hilo3.start();
+    }        
 }
